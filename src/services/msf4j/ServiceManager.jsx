3d83c74ee0e636d951e7330bec2247df86c38c56
pamodaaw <pamodaaw@gmail.com>
2018-05-09 09:23:54 +0530

    Change api endpoints

--- src/services/msf4j/ServiceManager.jsx
+++ src/services/msf4j/ServiceManager.jsx
@@ -32,7 +32,7 @@ class ServiceManager extends Component {
      * @returns {Promise<AxiosResponse<any>>}
      */
     getUploadedPacks() {
-        const url = MainData.microServiceURL + 'getPacks';
+        const url = MainData.microServiceURL + 'pack/list';
         const requestConfig = { withCredentials: true, timeout: 40000000 };
 
         return axios.get(url, requestConfig).then((response) => {
@@ -47,7 +47,7 @@ class ServiceManager extends Component {
      * @returns {Promise<AxiosResponse<any>>}
      */
     selectLicense() {
-        const url = MainData.microServiceURL + 'selectLicense';
+        const url = MainData.microServiceURL + 'license/list';
         const requestConfig = { withCredentials: true };
         return axios.get(url, requestConfig).then((response) => {
             return response;
@@ -61,9 +61,9 @@ class ServiceManager extends Component {
      * @returns {Promise<AxiosResponse<any>>}
      */
     getLicense() {
-        const url = MainData.microServiceURL + 'getLicense';
+        const url = MainData.microServiceURL + 'license/text';
         const requestConfig = { withCredentials: true, timeout: 40000000 };
-        return axios.get(url, requestConfig).then((response) => {
+        return axios.post(url, requestConfig).then((response) => {
             return response;
         }).catch((error) => {
             throw error
@@ -75,7 +75,7 @@ class ServiceManager extends Component {
      * @returns {Promise<AxiosResponse<any>>}
      */
     dowloadLicense() {
-        const url = MainData.microServiceURL + 'downloadLicense';
+        const url = MainData.microServiceURL + 'license/textToDownload';
         const requestConfig = { withCredentials: true, timeout: 30000 };
         return axios.get(url, requestConfig).then((response) => {
             return response;
@@ -90,7 +90,7 @@ class ServiceManager extends Component {
      * @returns {Promise<AxiosResponse<any>>}
      */
     extractJars(selectedPack) {
-        const url = MainData.microServiceURL + 'extractJars';
+        const url = MainData.microServiceURL + 'pack/jars';
         const requestConfig = {
             withCredentials: true,
         };
@@ -107,7 +107,7 @@ class ServiceManager extends Component {
      * @returns {Promise<AxiosResponse<any>>}
      */
     enterJars(data) {
-        const url = MainData.microServiceURL + 'enterJars';
+        const url = MainData.microServiceURL + 'pack/nameDefinedJars';
         const requestConfig = {
             withCredentials: true,
         };
@@ -128,7 +128,7 @@ class ServiceManager extends Component {
      * @returns {Promise<AxiosResponse<any>>}
      */
     addLicense(components, libraries) {
-        const url = MainData.microServiceURL + 'addLicense';
+        const url = MainData.microServiceURL + 'license/newLicenses';
         const requestConfig = {
             withCredentials: true,
         };
